ea52909813df5165fe6605901704ba12e86f4a31
ramarfx <ramagamiing33@gmail.com>
2024-03-21 11:39:05 +0700

    navbar

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,107 +1,28 @@
+import { Link } from "@inertiajs/react";
+import NavLink from "./NavLink";
 
+const Navbar = () => {
+  return (
+    <header className="w-full flex border-b border-border/40 sticky top-0 z-50 backdrop-blur">
+      <div className="container flex h-14 max-w-screen-2xl items-center justify-between">
 
-const custom = {
-    root: {
-        base: "bg-white px-2 py-2.5 sm:px-4 border-b",
-        rounded: {
-            on: "rounded",
-            off: "",
-        },
-        bordered: {
-            on: "border",
-            off: "",
-        },
-        inner: {
-            base: "mx-auto flex flex-wrap items-center justify-between",
-            fluid: {
-                on: "",
-                off: "container",
-            },
-        },
-    },
-    brand: {
-        base: "flex items-center",
-    },
-    collapse: {
-        base: "w-full md:block md:w-auto",
-        list: "mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium",
-        hidden: {
-            on: "hidden",
-            off: "",
-        },
-    },
-    link: {
-        base: "block text-base py-2 pr-4 pl-3 md:p-0",
-        active: {
-            on: "bg-cyan-700 text-white dark:text-white md:bg-transparent md:text-primary",
-            off: "border-b border-gray-100 text-primary/50 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-primary md:dark:hover:bg-transparent md:dark:hover:text-white",
-        },
-        disabled: {
-            on: "text-gray-400 hover:cursor-not-allowed dark:text-gray-600",
-            off: "",
-        },
-    },
-    toggle: {
-        base: "inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden",
-        icon: "h-6 w-6 shrink-0",
-    },
+        <div className="mr-4 flex items-center">
+          <h1 className="text-xl font-bold mr-12">MyKasir</h1>
+
+          <nav className="flex gap-6 text-sm">
+            <Link href="/">Home</Link>
+            <Link href="/">Product</Link>
+            <Link href="/">Cart</Link>
+          </nav>
+        </div>
+
+        <div className="flex items-center gap-6 text-sm">
+          <Link href="/login">Login</Link>
+          <Link href="/register">Register</Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
-export default function Component() {
-    return (
-        <Navbar fluid theme={custom}>
-            <NavbarBrand href="https://flowbite-react.com">
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-                    KASIR
-                </span>
-            </NavbarBrand>
-
-            <div className="flex md:order-2 items-center">
-                <ul className="md:mr-10 hidden md:flex mt-4 flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-bold">
-                    <NavbarLink href="#" active>
-                        Home
-                    </NavbarLink>
-                    <NavbarLink href="#">About</NavbarLink>
-                    <NavbarLink href="#">Services</NavbarLink>
-                    <NavbarLink href="#">Pricing</NavbarLink>
-                    <NavbarLink href="#">Contact</NavbarLink>
-                </ul>
-
-                <Dropdown
-                    arrowIcon={false}
-                    inline
-                    label={
-                        <Avatar
-                            alt="User settings"
-                            img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                            rounded
-                        />
-                    }
-                >
-                    <DropdownHeader>
-                        <span className="block text-sm">Bonnie Green</span>
-                        <span className="block truncate text-sm font-medium">
-                            name@flowbite.com
-                        </span>
-                    </DropdownHeader>
-                    <DropdownItem>Dashboard</DropdownItem>
-                    <DropdownItem>Settings</DropdownItem>
-                    <DropdownItem>Earnings</DropdownItem>
-                    <DropdownDivider />
-                    <DropdownItem>Sign out</DropdownItem>
-                </Dropdown>
-
-                <NavbarToggle />
-            </div>
-            <NavbarCollapse className="md:mr-5 md:hidden">
-                    <NavbarLink href="#" active>
-                        Home
-                    </NavbarLink>
-                    <NavbarLink href="#">About</NavbarLink>
-                    <NavbarLink href="#">Services</NavbarLink>
-                    <NavbarLink href="#">Pricing</NavbarLink>
-                    <NavbarLink href="#">Contact</NavbarLink>
-                </NavbarCollapse>
-        </Navbar>
-    );
-}
+export default Navbar;
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,29 +1,35 @@
+import Navbar from "@/Components/Navbar";
 import { Button } from "@/Components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/Components/ui/card";
+
 
 const Home = () => {
-    return (
-        <div>
-            {/* <Navbar />
+  return (
+    <div>
+      <Navbar />
 
-            <div className="flex">
-                <div className="grid grid-cols-4 p-2 gap-2 w-3/4">
-                    <Card image={"/a.webp"} />
-                    <Card image={"/a.webp"} />
-                    <Card image={"/a.webp"} />
-                    <Card image={"/a.webp"} />
-                    <Card image={"/a.webp"} />
-                    <Card image={"/a.webp"} />
-                    <Card image={"/a.webp"} />
-                    <Card image={"/a.webp"} />
-                </div>
-
-                <div className="w-1/4 border-l p-5">
-                    <h1 className="text-primary font-bold text-2xl">Keranjang</h1>
-                </div>
-            </div> */}
-            <Button key={1} variant="default">Button</Button>
+      <div className="flex">
+        <div className="grid grid-cols-4 p-2 gap-2 w-3/4">
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <Button>add</Button>
+            </CardFooter>
+          </Card>
         </div>
-    );
+
+        <div className="w-1/4 border-l border-border/40 p-5">
+          <h1 className="text-primary font-bold text-2xl">Keranjang</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
