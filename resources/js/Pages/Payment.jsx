544fd8601d6cd33dd321e7b05eb4d0949ecb67f6
ramarfx <ramagamiing33@gmail.com>
2024-03-28 09:56:59 +0700

    react to pdf

--- resources/js/Pages/Payment.jsx
+++ resources/js/Pages/Payment.jsx
@@ -1,15 +1,23 @@
-const payment = ({data}) => {
+import { Button } from "@/Components/ui/button";
+import { usePDF } from "react-to-pdf";
+
+const payment = ({ data }) => {
+  const {toPDF, targetRef} = usePDF({
+    filename: "invoice.pdf",
+  });
 
   console.log(data.payment);
 
   return (
-    <div>
+    <div ref={targetRef}>
       ini payment
       <p>{data.payment}</p>
       <p>{data.phone}</p>
       <p>{data.total}</p>
+
+      <Button onClick={() => toPDF()}>ini tombol pdf</Button>
     </div>
-   );
-}
+  );
+};
 
 export default payment;
